db0d638216971b2a399ad49cce6e38e7bae1c0d9
Phat Tien <phattien991412@gmail.com>
2023-07-29 00:17:19 +0700

    create function to top

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 
 import { AiOutlineArrowUp } from "react-icons/ai";
@@ -7,13 +7,37 @@ const Header = dynamic(() => import("./Header"));
 const Footer = dynamic(() => import("./Footer"));
 
 const Layout = ({ children }) => {
+  const [showButton, setShowButton] = useState(false)
+
+  const handleToTop = () => {
+    const toTop = document.getElementById("top");
+
+    if (toTop) {
+      toTop.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+
+  const handleShowButton = () => {
+    if (window.scrollY >= 800) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleShowButton);
+
+    return () => window.removeEventListener("scroll", handleShowButton);
+  }, []);
+
   return (
-    <div className=" max-w-screen-[100vw] overflow-x-hidden">
+    <div id="top" className=" max-w-screen-[100vw] overflow-x-hidden">
       <Header />
       <main>{children}</main>
       <div
-        // onClick={handleToTop}
-        className="totop opacity-1 grid place-items-center w-14 h-14 rounded-full bg-button text-white text-3xl fixed right-10 bottom-20 cursor-pointer hover:bg-[#8047cb]"
+        onClick={handleToTop}
+        className={`${showButton ? "right-10" : "-right-20"} totop opacity-1 grid place-items-center w-14 h-14 rounded-full bg-button text-white text-3xl fixed bottom-20 transition-all duration-500 cursor-pointer hover:bg-[#8047cb]`}
       >
         <AiOutlineArrowUp />
       </div>
